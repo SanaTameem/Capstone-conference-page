9e3d9bd83f9543c1710404af8f51b02e0a55619e
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-03-21 20:44:32 +0430

    Add about.js

---
+++ scripts/about.js
@@ -0,0 +1,19 @@
+// mobile menu :
+// const homepage = document.querySelector('.homepage');
+const moreBtnIcn = document.querySelector('.btnIcon');
+const closeBtn = document.querySelector('.close-btn');
+const humburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu-container');
+
+humburger.addEventListener('click', () => {
+  mobileMenu.style.display = "block";
+  moreBtnIcn.style.filter = 'blur(10px)';
+});
+closeBtn.addEventListener('click', ()=>{
+  mobileMenu.style.display = "none";
+  moreBtnIcn.style.filter = 'blur(0)';
+});
+mobileMenu.addEventListener('click', ()=>{
+  mobileMenu.style.display = "none";
+  moreBtnIcn.style.filter = 'blur(0)';
+});
--- scripts/index.js
+++ scripts/index.js
@@ -127,17 +127,20 @@ moreSpeakers.addEventListener('click', () => {
 
 // mobile menu :
 // const homepage = document.querySelector('.homepage');
-// const mainProgram = document.querySelector('.main-program');
+const moreBtnIcn = document.querySelector('.btnIcon');
 const closeBtn = document.querySelector('.close-btn');
 const humburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu-container');
 
 humburger.addEventListener('click', () => {
   mobileMenu.style.display = "block";
+  moreBtnIcn.style.filter = 'blur(10px)';
 });
 closeBtn.addEventListener('click', ()=>{
   mobileMenu.style.display = "none";
+  moreBtnIcn.style.filter = 'blur(0)';
 });
 mobileMenu.addEventListener('click', ()=>{
   mobileMenu.style.display = "none";
+  moreBtnIcn.style.filter = 'blur(0)';
 });
