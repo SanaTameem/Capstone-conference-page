59ddb19ce15f222deb9206d1f9b73f885f8a9f46
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-03-22 13:44:49 +0430

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -130,30 +130,28 @@ const moreBtnIcn = document.querySelector('.btnIcon');
 const closeBtn = document.querySelector('.close-btn');
 const humburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu-container');
-
+//Humburger event listener
 humburger.addEventListener('click', () => {
+  function checkViewportSize() {
+    if (window.matchMedia('(min-width: 786px)').matches) {
+      mobileMenu.style.display = 'none';
+      moreBtnIcn.style.filter = 'blur(0)';
+    } else {
+      mobileMenu.style.display = 'block';
+      moreBtnIcn.style.filter = 'blur(10px)';
+    }
+  }
+  window.addEventListener('resize', checkViewportSize);
   mobileMenu.style.display = 'block';
   moreBtnIcn.style.filter = 'blur(10px)';
 });
+//close Button event listener
 closeBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
   moreBtnIcn.style.filter = 'blur(0)';
 });
+//mobile menu event listener
 mobileMenu.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
   moreBtnIcn.style.filter = 'blur(0)';
 });
-
-// resize
-function checkViewportSize() {
-  if (window.matchMedia('(min-width: 786px)').matches) {
-    mobileMenu.style.display = 'none';
-    moreBtnIcn.style.filter = 'blur(0)';
-  } else {
-    mobileMenu.style.display = 'block';
-    moreBtnIcn.style.filter = 'blur(10px)';
-  }
-}
-
-window.addEventListener('resize', checkViewportSize, false);
-window.addEventListener('load', checkViewportSize, false);
